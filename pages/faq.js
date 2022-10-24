f590502c121c992c8d51068642c419138a2f7143
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-10-24 19:57:22 -0300

    Páginas e início de projeto

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from '../src/components/Link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Página de perguntas</h1>
+            <Link href="/" passHref >Ir para a Home</Link>
+        </div>
+    )
+}
