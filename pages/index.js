1510c8a2158b6fdb2a2c32419807ef12eb389315
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-10-24 20:21:09 -0300

    estilizando

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+import React from 'react';
 import Link from '../src/components/Link';
+
+function Title({children}){
+    return (
+        <React.Fragment>
+        <h1>
+            {children}
+        </h1>
+        <style jsx>
+            {`
+            h1 { 
+                color: red;
+                font-family: sans-serif;
+            }
+            `}
+        </style>
+        </React.Fragment>
+    );
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases</h1>
+            <Title>Alura Cases</Title>
             <Link href="/faq">
             Ir para a página do FAQ
             </Link>
