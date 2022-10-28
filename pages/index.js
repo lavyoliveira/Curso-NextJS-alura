64ff19cab291291c0829c5647df7410c9d3f98fb
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2022-10-28 16:43:01 -0300

    estilização e pastas

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,3 @@
-import React from 'react';
-import Link from '../src/components/Link';
+import HomeScreen from '../src/screens/HomeScreen';
 
-function Title({children}){
-    return (
-        <React.Fragment>
-        <h1>
-            {children}
-        </h1>
-        <style jsx>
-            {`
-            h1 { 
-                color: red;
-                font-family: sans-serif;
-            }
-            `}
-        </style>
-        </React.Fragment>
-    );
-}
-
-export default function HomePage() {
-    return (
-        <div>
-            <Title>Alura Cases</Title>
-            <Link href="/faq">
-            Ir para a página do FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen;
